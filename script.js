93f949845102c9b6eb6d186fcdc6084c53075fcd
yosin <umnovanika1@gmail.com>
2024-05-03 21:13:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let yosin = {
+  Name: "yosin",
+  LastName: "Fozilov",
+  age: 14,
+  isEmployed: false,
+  you: function () {
+    return "Hello to Yosin";
+  },
+};
+console.log(Object.values(yosin));
