807e67e1803e062dfe0b41affb87db6f0c63b575
yosin <umnovanika1@gmail.com>
2024-05-04 20:28:04 +0500

    DESTRUCTURE VALUE OF OBJECT

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let obj = {
-  Firstname: "Yosin",
-  Lastname: "Fozilov",
-  age: 14,
-  isEmployed: false,
-};
+// let obj = {
+//   Firstname: "Yosin",
+//   Lastname: "Fozilov",
+//   age: 14,
+//   isEmployed: false,
+// };
 
 //it uses for showing the keys and value wich object has in array format
 
@@ -28,3 +28,23 @@ let obj = {
 // console.log(Object.values(obj)); //OUTPUT: --> [ 'Yosin', 'Fozilov', 14, false ]
 
 // it uses for showing the values wich object has in array format
+
+//Destructuring method
+
+//DESTRUCTURE VALUE OF OBJECT
+// let person1 = {
+//   FirstName: "John",
+//   LastName: "Doe",
+//   age: 30,
+//   Job: "Programmer",
+// };
+// let person2 = {
+//   FirstName: "Yosin",
+//   LastName: "Fozilov",
+//   age: 14,
+// };
+
+// let { FirstName, LastName, Job = "Workless", age } = person2;
+// console.log(Job);
+// console.log(FirstName);
+// console.log(LastName);
